f7bc565f50dc1d1c49105c8a421ffdb9f36d2d96
Matt Gross <matt.gross@thetradedesk.com>
2025-04-17 18:38:15 -0600

    add third party data bulk script

---
+++ Python/ThirdPartyData/GetAllThirdPartyDataForPartnerBatchedGQL.py
@@ -0,0 +1,209 @@
+######################################################################
+# This script calls GQL to get the third party data for a partner.
+######################################################################
+
+import json
+import requests
+import time
+from typing import Any, List, Tuple
+
+###########
+# Constants
+###########
+
+# Define the GQL Platform API endpoint URLs.
+EXTERNAL_SB_GQL_URL = 'https://ext-desk.sb.thetradedesk.com/graphql'
+PROD_GQL_URL = 'https://desk.thetradedesk.com/graphql'
+
+#############################
+# Variables for YOU to define
+#############################
+
+# Define the GraphQL Platform API endpoint URL this script will use.
+gql_url = EXTERNAL_SB_GQL_URL
+
+# Replace the placeholder value with your actual API token.
+token = 'TOKEN_PLACEHOLDER'
+
+# Replace the placeholder with the ID of the partner you want to query third party data for.
+partner_id = 'PARTNER_ID_PLACEHOLDER'
+
+################
+# Helper Methods
+################
+
+# Represents a response from the GQL server.
+class GqlResponse:
+  def __init__(self, data: dict[Any, Any], errors: List[Any]) -> None:
+    # This is where the return data from the GQL operation is stored.
+    self.data = data
+    # This is where any errors from the GQL operation are stored.
+    self.errors = errors
+
+# Executes a GQL request to the specified gql_url, using the provided body definition and associated variables.
+# This indicates if the call was successful and returns the `GqlResponse`.
+def execute_gql_request(body, variables) -> Tuple[bool, GqlResponse]:
+  # Create headers with the authorization token.
+  headers: dict[str, str] = {
+    'TTD-Auth': token
+  }
+
+  # Create a dictionary for the GraphQL request.
+  data: dict[str, Any] = {
+    'query': body,
+    'variables': variables
+  }
+
+  # Send the GraphQL request.
+  response = requests.post(url=gql_url, json=data, headers=headers)
+  content = json.loads(response.content) if len(response.content) > 0 else {}
+
+  if not response.ok:
+    print('GQL request failed!')
+    # For more verbose error messaging, uncomment the following line:
+    #print(response)
+
+  # Parse any data if it exists, otherwise, return an empty dictionary.
+  resp_data = content.get('data', {})
+  # Parse any errors if they exist, otherwise, return an empty error list.
+  errors = content.get('errors', [])
+
+  return (response.ok, GqlResponse(resp_data, errors))
+
+
+# Schedules a query job to retrieve the partner's third party data. This returns the ID for the created job.
+def create_partner_third_party_data_job() -> str:
+  query = f'''query {{
+    partner(id: "{partner_id}") {{
+      thirdPartyData {{
+        nodes {{
+            id
+            name
+            providerId
+            providerElementId
+            description
+            allowCustomFullPath
+            buyable
+            dataAllianceExcluded
+            defaultSortScore
+            fullPath
+            hierarchyString
+            activeUniques {{
+                idsCount
+                householdCount
+                idsConnectedTvCount
+                idsInAppCount
+                idsWebCount
+                personsCount
+                thirdPartyDataOverlapCount
+                lastUpdated
+            }}
+        }}
+      }}
+    }}
+  }}'''
+
+  jobQuery = f'''
+  mutation CreatePartnerThirdPartyDataBulkQuery {{
+    createQueryBulk(
+      input: {{
+        query: """{query}"""
+      }}
+    ) {{
+      errors {{
+        ... on MutationError {{
+          message
+          field
+        }}
+        ... on BulkJobQueryValidationError {{
+          message
+          field
+          queryErrors
+        }}
+      }}
+      data {{
+        id
+      }}
+    }}
+  }}'''
+
+  # Send the GraphQL request.
+  request_success, response = execute_gql_request(jobQuery, {})
+
+  if not request_success:
+    print(response.errors)
+    raise Exception('Failed to schedule 3PD retrieval job.')
+
+  try:
+    id = response.data['createQueryBulk']['data']['id']
+
+    if not id:
+      print(response.errors)
+      raise('Could not create query job.')
+  except:
+    print(response.errors)
+    raise('Could not create query job.')
+
+  return id
+
+# Queries a given Partner's third party data and prints the result file URL.
+def query_partner_third_party_data() -> None:
+  job_id = create_partner_third_party_data_job()
+  status_query = f"""query GetBulkJobStatus {{
+    bulkJob(id: "{job_id}") {{
+        id
+        status
+        url
+        gqlErrors
+    }}
+  }}"""
+  should_poll = True
+
+  print('Waiting on data retrieval job...')
+
+  while should_poll:
+    time.sleep(5)
+
+    # Check the job state.
+    request_success, response = execute_gql_request(status_query, {})
+
+    if not request_success:
+      print(response.errors)
+      raise Exception('Failed to query 3PD retrieval job.')
+
+    status = response.data['bulkJob']['status']
+    should_poll = status == 'QUEUED' or status == 'IN_PROGRESS'
+
+    # If the job completed:
+    #   - In the case of success, print the URL.
+    #   _ In the case of failure, print the errors.
+    if not should_poll:
+      url = response.data['bulkJob']['url']
+
+      if not url:
+        print('Query job failed with errors:')
+        print(response.data['bulkJob']['gqlErrors'])
+      else:
+        download_output_file(url)
+
+      return
+
+# Downloads a given URL to a local file.
+def download_output_file(url: str):
+  local_filename = 'tpd.json'
+
+  with requests.get(url, stream=True) as r:
+      r.raise_for_status()
+      with open(local_filename, 'wb') as f:
+          for chunk in r.iter_content(chunk_size=8192):
+              f.write(chunk)
+
+  print(f"Downloaded 1PD to file: {local_filename}")
+
+###########################################################
+# Execution Flow:
+#  1. Query the partner ID specified for third party data.
+#  2. Wait on the query job to finish.
+#  3. Output the URL to the file containing the results.
+###########################################################
+query_partner_third_party_data()
